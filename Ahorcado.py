46f87662bc14d1cc8f3879cab8983b9705eba759
Marcelo8985 <marceloyugcha986@gmail.com>
2024-08-04 20:44:53 -0500

    actualice  ahorcado
    actualice mi juego del ahorcado

--- Ahorcado.py
+++ Ahorcado.py
@@ -1,6 +1,12 @@
+#importe la libreria random para mi juego
+import random
+#se ha creado el inicio del juego
 print("Bienvenido al juego del ahorcado")
 Nombre = input("Ingrese su nombre")
 print(f"Bienvenido {Nombre} evita morir")
 NumeroVIdas= 6
-Palabra= "Locura"
-print(Palabra)
+Palabras= ["jojo", "caballo", "loco", "ingeniero", "software"]
+secreto= random.choice(Palabras)
+#este print es para verificar que la variable secreto funciona posteriormente se la quitara
+print(secreto)
+#Desconozco aun mucho de python por lo que solo puedo avanzar hasta aqui, espero y en proximas clases pueda avanzar mas
